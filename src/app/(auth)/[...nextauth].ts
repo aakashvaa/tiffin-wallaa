f19e5008fc2dcc70ce722266ec2c36aa677b87fb
AkashKumar-0 <ahmvaad@gmail.com>
2024-11-29 01:20:37 +0530

    soved substring problem by upgrading to next auth beta

--- src/app/(auth)/[...nextauth].ts
+++ src/app/(auth)/[...nextauth].ts
@@ -1,15 +1,2 @@
-import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-});
+import { handlers } from '@/auth';
+export const { GET, POST } = handlers;
